17fbe1061afdf0ab8f0876e55ab608897898588f
Syco3514 <sycox3514@gmail.com>
2026-07-19 13:50:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,40 @@
-from fastapi import FastAPI
+from database import init_db
 
-from database import *
-from collector import *
-
-from logic_engine import *
-from logic_engine import weighted_vote
-
-
-
+# Database sab se pehle initialize
 init_db()
 
 
-app=FastAPI()
+from fastapi import FastAPI
+from collector import get_history
+from logic_engine import weighted_vote
 
+
+app = FastAPI()
 
 
 @app.get("/")
 def home():
-
     return {
-    "status":"Wingo AI Version 3"
+        "status": "Wingo AI Version 3 Running"
     }
-
 
 
 @app.get("/predict")
 def predict():
 
+    history = get_history()
 
-    history=get_history()
-
-
-    result=weighted_vote(history)
-
-
+    result = weighted_vote(history)
 
     return {
-
-
-    "history":history,
-
-
-    "weighted_prediction":result
-
-
+        "history": history,
+        "weighted_prediction": result
     }
-
 
 
 @app.get("/stats")
 def stats():
+
+    from database import get_stats
 
     return get_stats()
