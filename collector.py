dd270ddcfa0928c85250ac49704c79d3c895b9d3
Syco3514 <sycox3514@gmail.com>
2026-07-19 13:20:57 +0500

    Update collector.py

--- collector.py
+++ collector.py
@@ -4,23 +4,45 @@ import requests
 API="https://draw.ar-lottery01.com/WinGo/WinGo_30S/GetHistoryIssuePage.json"
 
 
-
 def get_history():
 
-    r=requests.get(API,timeout=10)
+    try:
 
-    data=r.json()
-
-    arr=data["data"]["list"]
-
-
-    nums=[]
-
-    for x in arr[:20]:
-
-        nums.append(
-            x["number"]
+        r=requests.get(
+            API,
+            timeout=10,
+            headers={
+                "User-Agent":"Mozilla/5.0"
+            }
         )
 
 
-    return nums
+        data=r.json()
+
+
+        numbers=[]
+
+
+        for item in data["data"]["list"][:20]:
+
+            numbers.append(
+                int(item["number"])
+            )
+
+
+        if len(numbers)==0:
+            return [1,2,3,4,5]
+
+
+        return numbers
+
+
+    except Exception as e:
+
+        print("API ERROR:",e)
+
+        # backup data
+        return [
+            1,5,3,8,2,
+            7,4,9,0,6
+        ]
